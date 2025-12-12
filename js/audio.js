e506b40014522181ff9eed5b2eb78b985be9798f
Lori-77 <lori2336zi@gmail.com>
2025-12-12 10:27:29 +0800

    Update audio.js

--- js/audio.js
+++ js/audio.js
@@ -49,13 +49,13 @@ var musicId = 0;
 
 // 初始化音乐
 function initMusic() {
-    audio.src = `./mp3/music${musicId}.mp3`;
+    audio.src = `../mp3/music${musicId}.mp3`;
     audio.load();
     recordImg.classList.remove('rotate-play');
     
     // 当音乐的元数据完成加载时触发下面这个函数：
     audio.onloadedmetadata = function() {
-        recordImg.style.backgroundImage = `url('./img/record${musicId}.jpg')`;
+        recordImg.style.backgroundImage = `url('../img/record${musicId}.jpg')`;
         body.style.backgroundImage = `url('./img/bg${musicId}.png')`; 
         musicTitle.innerText = musicData[musicId][0];
         authorName.innerText = musicData[musicId][1];
@@ -153,7 +153,7 @@ playMode.addEventListener('click', function() {
     if (modeID > 3) {
         modeID = 1;
     }
-    playMode.style.backgroundImage = `url('./img/mode${modeID}.png')`;
+    playMode.style.backgroundImage = `url('../img/mode${modeID}.png')`;
 });
 
 // 当音乐播放完
@@ -205,9 +205,9 @@ function updateVolume() {
 // 更新音量图标的函数
 function updateVolumnIcon() {
     if (audio.muted || audio.volume == 0) {
-        volume.style.backgroundImage = `url('./img/静音.png')`;
+        volume.style.backgroundImage = `url('../img/静音.png')`;
     } else {
-        volume.style.backgroundImage = `url('./img/音量.png')`;
+        volume.style.backgroundImage = `url('../img/音量.png')`;
     }
 }
 
@@ -260,3 +260,4 @@ function createMusic() {
 
 
 document.addEventListener('DOMContentLoaded', createMusic);
+
